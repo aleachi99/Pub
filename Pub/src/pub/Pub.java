6210bfffba8f870e42c648c12e5babb0a129b2ab
rossi.alessandroachi <rossi.alessandroachi@TAVE30.OSDBSESTO.local>
2017-04-08 11:35:11 +0200

    Creata classe Product

---
+++ Pub/src/pub/Pub.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pub;
+
+/**
+ *
+ * @author rossi.alessandroachi
+ */
+public class Pub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
